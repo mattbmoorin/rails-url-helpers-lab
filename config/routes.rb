8bbfe4a4ed5c061f67aaa6efa3fda527c9771d20
mattbmoorin <tzaratape@gmail.com>
2021-01-17 23:46:04 -0600

    learn test being weird

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources :students, to: [:index, :show]
   get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
